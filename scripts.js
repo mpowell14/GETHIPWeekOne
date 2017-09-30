c543bcaf342194ac3672f21967e6d3236df031a8
Powell <Melanie_Powell@gallup.com>
2017-09-30 11:38:31 -0500

    added button to randomly change background color

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 function displayName() {
     document.getElementById("demo").innerHTML = document.getElementById('firstname').value + " " + document.getElementById('lastname').value;
 }
+
+function changeBodyColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
